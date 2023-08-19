8b2dc117afc8bdc69da756473d76138d25d3800c
Priyadarshini <priyadar2001@gmail.com>
2023-08-19 21:06:55 +0530

    navbar

---
+++ FrontEnd/script.js
@@ -0,0 +1,10 @@
+function toggleDropdown(){
+    var dropdownOptions = document.getElementById("dropdownOptions");
+    if(dropdownOptions.style.display == "block") {
+        dropdownOptions.style.display = "none";
+    }
+    else {
+        dropdownOptions.style.display = "block";
+    }
+
+}
